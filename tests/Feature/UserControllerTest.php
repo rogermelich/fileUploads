04fed9cdef9b34f4b8fb1da97e3a6fc76465afca
Roger Melich <rogermelich@gmail.com>
2017-05-16 19:53:19 +0200

    First version test

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Faker\Factory;
+use Illuminate\Http\UploadedFile;
+use Storage;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUsersAddCreatedTest()
+    {
+        // Prepare
+        Storage::fake('general');
+        $faker = factory::create();
+        $user = [
+            'name' => $name = $faker->name,
+            'email' => $email = $faker->email,
+            'password' => $password = bcrypt('secret'),
+            'file' => $file = UploadedFile::fake()->image('file.png')];
+        //Login as autorized
+        $authorizeUser = factory(User::class)->create();
+        // Execute
+        $response = $this->actingAs($authorizeUser, 'api')->json('POST', 'api/v1/user', $user);
+
+        // Assert
+        $response
+            ->assertStatus(200)
+            ->assertExactJson([
+                'created' => true,
+            ]);
+
+        $this->assertDatabaseHas('users', [
+            'name'      => $name,
+            'email'     => $email,
+            'password'  => $password,
+            'file'      => $file
+        ]);
+    }
+}
