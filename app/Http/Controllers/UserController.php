bb049bff30eb98af22c7f6c3b59324d43cf0414f
Roger Melich <rogermelich@gmail.com>
2017-05-16 20:09:12 +0200

    Add file, nam, ...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use SebastianBergmann\CodeCoverage\Report\Html\File;
 
 class UserController extends Controller
 {
@@ -35,6 +36,23 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        dump($request->input('file'));
+
+        $file = $request->file('file');
+        dump($file);
+
+        $name = $file->getClientOriginalName();
+        dump($name);
+
+        Storage::disk('local')->put($name, File::get($file));
+
+        $databaseUser = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+            'file' => $request->input('file')
+        ];
+        dump($databaseUser);
         User::create($request->only(['name', 'email', 'password', 'file']));
 
         return ['created' => true];
